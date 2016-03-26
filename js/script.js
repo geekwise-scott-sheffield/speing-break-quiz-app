620f38eda512d6ce1d458ca09a05eceb77b41236
Scott B. Sheffield <geekwise.scott.sheffield@gmail.com>
2016-03-25 17:08:34 -0700

    minor-touchups-&-style-edits

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-var correct = document.querySelectorAll(".answerOpts");
-var wrong = document.querySelectorAll(".answerOpts");
-
-
-function indexChoiceExample(){
-    correct[0].style.backgroundColor = "limegreen";
-    correct[0].textContent = "right answer";
-    wrong[1].style.backgroundColor = "crimson";
-    wrong[1].textContent = "wrong answer";
-    wrong[2].style.backgroundColor = "crimson";
-    wrong[2].textContent = "wrong answer";
-    wrong[3].style.backgroundColor = "crimson";
-    wrong[3].textContent = "wrong answer";
-}
-
-function showNext(){
-    document.querySelector("#hidePlaceHolder").style.display = "none";
-    document.querySelector("#nextQ").style.display = "block";
-}
+//var correct = document.querySelectorAll(".answerOpts");
+//var wrong = document.querySelectorAll(".answerOpts");
+//
+//
+//function indexChoiceExample(){
+//    correct[0].style.backgroundColor = "limegreen";
+//    correct[0].textContent = "right answer";
+//    wrong[1].style.backgroundColor = "crimson";
+//    wrong[1].textContent = "wrong answer";
+//    wrong[2].style.backgroundColor = "crimson";
+//    wrong[2].textContent = "wrong answer";
+//    wrong[3].style.backgroundColor = "crimson";
+//    wrong[3].textContent = "wrong answer";
+//}
+//
+//function showNext(){
+//    document.querySelector("#hidePlaceHolder").style.display = "none";
+//    document.querySelector("#nextQ").style.display = "block";
+//}
 
 
 //var questionArray = ["question5/7", "question2", "question3", "question4", "question5" ];
@@ -36,6 +36,7 @@ var quizArray = [
     "Trolls, in the internet webcomic Homestuck, are easily identified by their candy cane horns and ___ colored skin",
     "the comment based sit ___ resorted to DDOS attacks in a flame war over the internet personality Boxxy"
 ];
+var answerOptions = document.querySelectorAll(".answerOpts");
 
 
 //function is run if user selects wrong answer
@@ -51,24 +52,47 @@ function rightAnswer(){
     document.querySelector("#nextQ").style.display = "block";
 }
 
-//targets the possible answer buttons in html
-var yes = document.querySelectorAll(".answerOpts");
-var no = document.querySelectorAll(".answerOpts");
-
-
 //unique question and answer set for question 1
-function question1(){document.querySelector("#question1").textContent = quizArray[0];}
+function question1(){
+    document.querySelector("#question1").textContent = quizArray[0];
+    answerOptions[0].disabled = false;
+    answerOptions[1].disabled = false;
+    answerOptions[2].disabled = false;
+    answerOptions[3].disabled = false;
+}
 
 //unique question and answer set for question 2
-function question2(){document.querySelector("#question2").textContent = quizArray[1];}
+function question2(){
+    document.querySelector("#question2").textContent = quizArray[1];
+    answerOptions[0].disabled = false;
+    answerOptions[1].disabled = false;
+    answerOptions[2].disabled = false;
+    answerOptions[3].disabled = false;
+}
 
 //unique question and answer set for question 3
-function question3(){document.querySelector("#question3").textContent = quizArray[2];}
+function question3(){
+    document.querySelector("#question3").textContent = quizArray[2];
+    answerOptions[0].disabled = false;
+    answerOptions[1].disabled = false;
+    answerOptions[2].disabled = false;
+    answerOptions[3].disabled = false;
+}
 
 //unique question and answer set for question 4
-function question4(){document.querySelector("#question4").textContent = quizArray[3];}
+function question4(){
+    document.querySelector("#question4").textContent = quizArray[3];
+    answerOptions[0].disabled = false;
+    answerOptions[1].disabled = false;
+    answerOptions[2].disabled = false;
+    answerOptions[3].disabled = false;
+}
 
 //unique question and answer set for question 5
-function question5(){document.querySelector("#question5").textContent = quizArray[4];}
-
-
+function question5(){
+    document.querySelector("#question5").textContent = quizArray[4];
+    answerOptions[0].disabled = false;
+    answerOptions[1].disabled = false;
+    answerOptions[2].disabled = false;
+    answerOptions[3].disabled = false;
+}
